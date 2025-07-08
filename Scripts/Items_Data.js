c843bf7bee985ea117b1db8f2193652df67e352c
kevin Foendoe <kfoendoe1@gmail.com>
2025-07-07 22:32:31 -0400

    Adding image links

--- Scripts/Items_Data.js
+++ Scripts/Items_Data.js
@@ -3,7 +3,7 @@ var Items_JSON = [
     {
         Item_Name: "Double Cheese Burger",
         Item_Price: "13.20",
-        Img_Url: "/Media/ItemPictures/Burger.png"
+        Img_Url: "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Double-Cheeseburger:product-header-mobile?wid=768&hei=441&dpr=off"
     },
     {
         Item_Name: "Pastechi di keshi",
@@ -13,236 +13,236 @@ var Items_JSON = [
     {
         Item_Name: "Busta Lila",
         Item_Price: "2",
-        Img_Url: "/Media/ItemPictures/BoneMarrow.png"
+        Img_Url: "https://www.shopmassystoresbb.com/wp-content/uploads/2021/04/0003361318906.jpg"
     },
     {
         Item_Name: "Movie Ticket - Djaluna Sambil",
         Item_Price: "8",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: "https://media.istockphoto.com/id/862687118/vector/two-red-cinema-vector-tickets.jpg?s=612x612&w=0&k=20&c=5OS4Ad61iZDBdZFjei4hH9ImPqSAlEyugE-iKCivvnM="
     },
     {
         Item_Name: "Nike Shoes GT Cut 3",
         Item_Price: "156",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: "https://www.basketballemotion.com/imagesarticulos/232912/grandes/zapatillas-nike-gt-schnitt-3-azul-0.webp"
     },
     {
         Item_Name: "Apple AirPods Pro 4",
         Item_Price: "258",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/airpods-4-hero-select-202409_FMT_WHH?wid=752&hei=636&fmt=jpeg&qlt=90&.v=NFNYUnZEZG9WdmVKZzdXRW9IVzNoSXFGRDgrV1lYUUJja0JzaUZERUtaV0xlT0luZStNbmdWL2hzZnEwaHZYMnNudUNUdVBZcDJSbEgrd2pPbDFhNVk4Y0dKOSsweEZka1JxVzk5SFNyRjIrUm8xd3BPWU43Z0lDcmhIa0FsS0o"
     },
     {
         Item_Name: "PlayStation 5",
         Item_Price: "1398.99",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: "https://media.gamestop.com/i/gamestop/11108140?$pdp$"
     },
     {
         Item_Name: "DJI Mavic 4 Pro",
         Item_Price: "5379.95",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: "https://cdn11.bigcommerce.com/s-3sy6wxm/images/stencil/1280x1280/products/889/5393/m4-A__28512.1747165119.jpg?c=2"
     },
     {
         Item_Name: "Iphone 11 ",
         Item_Price: "212.90",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Iphone 16 Pro Max",
         Item_Price: "1599.97",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "MacBook Pro 14",
         Item_Price: "1599.97",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Apple Vision Pro",
         Item_Price: "3499",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "apple watch series 9",
         Item_Price: "402.22",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Designer Handbag",
         Item_Price: "5500",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Terrunyo Sauvignon Blanc 2022",
         Item_Price: "450",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Rolex - Sky-Dweller. Oyster, 42 mm, Oystersteel and white gold",
         Item_Price: "16600.00",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "calatrava patek philippe",
         Item_Price: "116050",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Tesla Model S Plaid",
         Item_Price: "89990",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Tesla Cybertruck",
         Item_Price: "81000",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "lamborghini huracán sterrato",
         Item_Price: "278,972",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Kia Rio 2018",
         Item_Price: "17500.23",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Nissan Sentra 2023",
         Item_Price: "21600",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Gold Bar 1kg",
         Item_Price: "108648.61",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Yacht - Sunseeker Predator 50",
         Item_Price: "1,563,293.28",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "F16",
         Item_Price: "68000000",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Boeing 747",
         Item_Price: "148000000",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Mona Lisa",
         Item_Price: "860000000",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Cruise Ship",
         Item_Price: "930000000",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Soi 95 - Dinner for two",
         Item_Price: "160.67",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Soi 95 - Dinner for two - But yall both big backs",
         Item_Price: "200.88",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Nobu Dinner for 3 - No tip",
         Item_Price: "484.78",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Mansion blue bay - 3 bedrooms",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Mansion zuurzak - 5 bedrooms",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Kas di maishi - 1 bedroom",
         Item_Price: "1300",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Hermes Birkin Bag",
         Item_Price: "22891.55",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Louboutin Red Bottoms",
         Item_Price: "3890,75",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Aqualectra - Monthly Bill",
         Item_Price: "150",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Flow Interner- Monthly Bill - 15% Discount",
         Item_Price: "122",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "7 Days in Corendon",
         Item_Price: "6146",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Fill up gas tank",
         Item_Price: "85.50",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "Koi lechi",
         Item_Price: "1.20",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "7 day stay at Sandals Resort",
         Item_Price: "14544",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
     {
         Item_Name: "",
         Item_Price: "",
-        Img_Url: "/Media/ItemPictures/SaltBae.png"
+        Img_Url: ""
     },
 ];
